cd9fd0c8fd6f01b4783a64d01a95cc80968ecb6b
AbdullaAhmed20 <162612898+AbdullaAhmed20@users.noreply.github.com>
2025-06-28 14:55:12 +0300

    Update App.h

--- App.h
+++ App.h
@@ -1,15 +1,3 @@
-/*#ifndef APP_H
-#define APP_H
-
-#include <wx/wx.h>
-
-class App : public wxApp {
-public:
-    virtual bool OnInit();
-};
-
-#endif*/
-
 #ifndef CONNECTFOUR_APP_H
 #define CONNECTFOUR_APP_H
 
