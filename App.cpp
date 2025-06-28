ee392d09e740bfa30a526ed8590ccd616aad8f3a
AbdullaAhmed20 <162612898+AbdullaAhmed20@users.noreply.github.com>
2025-06-28 14:53:57 +0300

    Update App.cpp

--- App.cpp
+++ App.cpp
@@ -1,16 +1,3 @@
-/*#include "App.h"
-#include "MainFrame.h"
-
-wxIMPLEMENT_APP(App);
-
-bool App::OnInit() {
-    MainFrame* mainFrame = new MainFrame("Tic-Tac-Toe");
-    mainFrame->Center();
-    mainFrame->Show();
-    return true;
-}*/
-
-
 #include "App.h"
 #include "MainFrame.h"
 
